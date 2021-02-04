152625d3689747210f7b4907c6213ecbe06a2ec3
just-hack <69382688+just-hack@users.noreply.github.com>
2021-02-04 15:55:10 +0530

    Update signapk_alpine.sh

--- signapk_alpine.sh
+++ signapk_alpine.sh
@@ -4,7 +4,7 @@ KEYTOOL="/usr/lib/jvm/java-1.8-openjdk/bin/keytool"
 JARSIGNER="/usr/lib/jvm/java-1.8-openjdk/bin/jarsigner"
 
 if [ ! -f $HOME/ash.keystore ]; then
-    $KEYTOOL -noprompt -dname "CN=mqttserver.ibm.com, OU=ID, O=IBM, L=Hursley, S=Hants, C=GB" -storepass hax4us -genkey -v -keystore $HOME/apkmod.keystore -alias hax4us -keyalg RSA -validity 10000 -keypass hax4us
+    $KEYTOOL -noprompt -dname "CN=mqttserver.ibm.com, OU=ID, O=IBM, L=Hursley, S=Hants, C=GB" -storepass Ashish -genkey -v -keystore $HOME/ash.keystore -alias Ashish -keyalg RSA -validity 10000 -keypass hax4us
 fi
 
 exec $JARSIGNER "$@"
